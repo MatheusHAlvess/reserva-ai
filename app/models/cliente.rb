4190658e7e207b1420b1460490d8fdba92e7f12c
genarioazevedoufape <genario.azevedo@ufape.edu.br>
2023-07-31 21:02:36 -0300

    Scaffold de Cliente

---
+++ app/models/cliente.rb
@@ -0,0 +1,8 @@
+class Cliente < ApplicationRecord
+  validates :nome_completo, presence: true
+  validates :data_nascimento, presence: true
+  validates :cpf, presence: true, uniqueness: true,
+            format: { with: /\A\d{11}\z/, message: 'deve conter 11 dígitos numéricos' }
+  validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+
+end
