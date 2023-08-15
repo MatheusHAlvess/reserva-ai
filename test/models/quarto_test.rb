50763d95a486201ca7a63cc3399e46d34c072a66
william <williambatista958@gmail.com>
2023-08-15 18:37:28 -0300

    Criando testes de model de gerenciamento de quarto

--- test/models/quarto_test.rb
+++ test/models/quarto_test.rb
@@ -1,7 +1,42 @@
 require "test_helper"
 
 class QuartoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "Criar um quarto válido" do
+    quarto = Quarto.new(
+      numero: 101,
+      preco: 100.0,
+      capacidade: 2
+    )
+
+    assert quarto.save
+  end
+
+  test "Editar um quarto" do
+    quarto = Quarto.create(
+      numero: 101,
+      preco: 100.0,
+      capacidade: 2
+    )
+    quarto.save()
+    novo_preco = 120.0
+    quarto.update(preco: novo_preco)
+    quarto.save()
+
+    assert_equal novo_preco, quarto.reload.preco
+  end
+
+  test "Remover um quarto" do
+    quarto = Quarto.create(
+      numero: 101,
+      preco: 100.0,
+      capacidade: 2
+    )
+    quarto.save()
+    id_quarto = quarto.id
+    quarto.destroy
+
+    assert_not Quarto.exists?(id_quarto)
+  end
+
 end
