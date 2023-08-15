d59c9fa2107abb45640ac78bb9e70ab0f8d04a72
william <williambatista958@gmail.com>
2023-08-15 18:38:39 -0300

    Criando testes de model de busca de quartos

--- test/models/quarto_test.rb
+++ test/models/quarto_test.rb
@@ -39,4 +39,40 @@ class QuartoTest < ActiveSupport::TestCase
     assert_not Quarto.exists?(id_quarto)
   end
 
+  test "Filtrar quartos por faixa de preço" do
+    quarto1 = Quarto.create(numero: 101, preco: 101.0, capacidade: 2)
+    quarto2 = Quarto.create(numero: 102, preco: 200.0, capacidade: 5)
+    quarto3 = Quarto.create(numero: 103, preco: 52.0, capacidade: 1)
+
+    resultado = Quarto.filtrar_quartos(nil, nil, "51-100")
+
+    assert_not_includes resultado, quarto1
+    assert_not_includes resultado, quarto2
+    assert_includes resultado, quarto3
+  end
+
+  test "Filtrar quartos por quantidade de hóspedes" do
+    quarto1 = Quarto.create(numero: 101, preco: 100.0, capacidade: 2)
+    quarto2 = Quarto.create(numero: 102, preco: 200.0, capacidade: 5)
+    quarto3 = Quarto.create(numero: 103, preco: 50.0, capacidade: 1)
+
+    resultado = Quarto.filtrar_quartos(nil, "0-4", nil)
+
+    assert_includes resultado, quarto1
+    assert_not_includes resultado, quarto2
+    assert_includes resultado, quarto3
+  end
+
+  test "Filtrar quartos por número" do
+    quarto1 = Quarto.create(numero: 101, preco: 100.0, capacidade: 2)
+    quarto2 = Quarto.create(numero: 102, preco: 200.0, capacidade: 5)
+    quarto3 = Quarto.create(numero: 103, preco: 50.0, capacidade: 1)
+
+    resultado = Quarto.filtrar_quartos(101, nil, nil)
+
+    assert_includes resultado, quarto1
+    assert_not_includes resultado, quarto2
+    assert_not_includes resultado, quarto3
+  end
+
 end
